c72486a6a55d7143ac5e8d35fe3f9c0faa71a45c
Diego Rocha <diego.dpr@hotmail.com>
2022-11-14 13:04:51 -0300

    start project, sidebar create

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Diego" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia incidunt suscipit quod quo tempore optio culpa, aut ipsum labore aperiam! Quod!"
+          />
+          <Post 
+            author="Teste" 
+            content="Novo Quia incidunt suscipit quod quo tempore optio culpa, aut ipsum labore aperiam! Quod!"
+          />
+        </main>
+      </div>
+    </>
+  )
+}
